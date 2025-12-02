6571a650f9c6251a466740d1aff3e2665c5cb544
Brian Roberto Sugira <brianrobertosugira@Roberto-9952.local>
2025-12-02 02:14:33 -0600

    Updated sword script and added slash animation controller

--- Assets/_Scripts/Sword.cs
+++ Assets/_Scripts/Sword.cs
@@ -2,23 +2,29 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    [SerializeField] private GameObject slashAnimPrefab;
+    [SerializeField] private Transform slashAnimSpawnPoint;
     private PlayerControlls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+
+    private GameObject slashAnim;
 
     private void Awake()
     {
         myAnimator = GetComponent<Animator>();
         playerControls = new PlayerControlls();
+        // Try to find a PlayerController on this object or a parent, otherwise fall back to any in the scene.
+        playerController = GetComponentInParent<PlayerController>();
+        if (playerController == null)
+        {
+            playerController = FindObjectOfType<PlayerController>();
+        }
     }
 
-    // private void OnEnable()
-    // {
-    //     // playerControls.Combat.Attack.started += _=> Attack();
-    // }
-
-    void Update()
+    private void Update()
     {
-         if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Attack();
         }
@@ -26,6 +32,50 @@ public class Sword : MonoBehaviour
 
     private void Attack()
     {
-        myAnimator.SetTrigger("Attack");
+        // Trigger animator if present
+        if (myAnimator != null)
+        {
+            myAnimator.SetTrigger("Attack");
+        }
+
+        // Spawn slash animation prefab at the spawn point
+        if (slashAnimPrefab != null && slashAnimSpawnPoint != null)
+        {
+            slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
+
+            // Apply facing flip if needed
+            var sr = slashAnim.GetComponent<SpriteRenderer>();
+            if (sr != null && playerController != null && playerController.FacingLeft)
+            {
+                sr.flipX = true;
+            }
+        }
+    }
+
+    public void SwingUpFLipAnim()
+    {
+        if (slashAnim == null) return;
+
+        slashAnim.transform.rotation = Quaternion.Euler(-180, 0, 0);
+
+        if (playerController != null && playerController.FacingLeft)
+        {
+            var sr = slashAnim.GetComponent<SpriteRenderer>();
+            if (sr != null) sr.flipX = true;
+        }
+    }
+
+    public void SwingDownFlipAnim()
+    {
+        if (slashAnim == null) return;
+
+        slashAnim.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        if (playerController != null && playerController.FacingLeft)
+        {
+            var sr = slashAnim.GetComponent<SpriteRenderer>();
+            if (sr != null) sr.flipX = true;
+        }
     }
 }
+
