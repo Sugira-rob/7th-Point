58024a0a82f9d946b4141b049ca42019873706a7
Brian Roberto Sugira <brianrobertosugira@Roberto-9952.local>
2025-12-04 14:06:17 -0600

    Added the death thing

--- Assets/_Scripts/DamageSource.cs
+++ Assets/_Scripts/DamageSource.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class DamageSource : MonoBehaviour
 {
+    [SerializeField] private int damageAmount = 1;
     // This class can be expanded with damage properties later
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<EnemyAI>() != null)
+        if (other.gameObject.GetComponent<EnemyHealth>() != null)
         {
-            // Example: Log when something is hit
-            Debug.Log($"{gameObject.name} hit {other.gameObject.name}");
-        }
+            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+            // Example: Deal 1 damage to the enemy
+            enemyHealth.TakeDamage(damageAmount);
     }
         
+}
+
 }
